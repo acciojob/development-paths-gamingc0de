3db73af3092b9ffb67c11320c8255b73eb69fa0b
gamingc0de <108025153+gamingc0de@users.noreply.github.com>
2023-09-10 16:49:17 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-<script>
-        // Select the list items
-        var listItems = document.querySelectorAll('#development-paths li');
-
-        // Append "Development" to each list item
-        listItems.forEach(function(item) {
-            item.textContent += 'Development';
-        });
-    </script>
